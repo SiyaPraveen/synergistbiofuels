fa4dc2fdd65c65ce83842f7f6655fb3c78990056
SiyaPraveen <siyapraveendas@gmail.com>
2023-04-11 18:53:17 +0530

    contact form sample

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 import './App.css';
 
 function App() {
+  const form = useRef();
+  const sendMail = (e)=>{
+    e.preventDefault();
+    emailjs.sendForm(
+      "service_jrraleh",
+      "template_j2mf8ty",
+      form.current,
+      "ycKqWWDhL3gVt6Rlv",
+
+    ).then(
+      result=>console.log(result),
+      error=>console.log(error)
+    )
+  }
   return (
     <div className="App">
       
@@ -11,11 +26,21 @@ function App() {
         </p>
         <span
           className="App-link" 
-          rel="noopener noreferrer"
+          rel="noopener noreferrer" 
         >
          Drop By Again Soon
         </span>
       </header>
+
+      <form ref={form} onSubmit={sendMail}>
+          <label>Name</label>
+          <input type="text" name="user_name" required />
+          <label>Email</label>
+          <input type="email" name="user_email" required />
+          <label>Message</label>
+          <textarea name="message" required></textarea>
+          <input type="submit" value="send"/>
+      </form>
     </div>
   );
 }
