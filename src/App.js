f0c1fb15af4d385b1ae7da1e86bae22a834516e4
SiyaPraveen <siyapraveendas@gmail.com>
2023-04-27 17:05:20 +0530

    fixing warning

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faEnvelopesBulk, faMapLocationDot, faPhoneSquare } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelopesBulk, faMapLocationDot, faPhoneSquare } from '@fortawesome/free-solid-svg-icons'
 
 import './App.css';
 
